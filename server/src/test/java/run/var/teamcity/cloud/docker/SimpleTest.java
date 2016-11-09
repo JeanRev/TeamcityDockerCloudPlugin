bf30879851b6550785f42f1c50a90eb11d8f8447
Jean Revertera <jean.revertera@gmail.com>
2016-11-09 08:50:55 +0100

    Test NG support

--- server/src/test/java/run/var/teamcity/cloud/docker/SimpleTest.java
+++ server/src/test/java/run/var/teamcity/cloud/docker/SimpleTest.java
@@ -1,9 +1,11 @@
 package run.var.teamcity.cloud.docker;
 
+import org.testng.annotations.Test;
 
+@Test
 public class SimpleTest {
 
-    public void testSomething() {
+    public void doSomething() {
 
     }
 }
