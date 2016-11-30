f0abf2a553531249d741a003fd0148cb898cba67
Jean Revertera <jean.revertera@gmail.com>
2016-11-30 08:35:41 +0100

    Improved handling of official image resolution
    - We use now the server whole version string including macro number to resolve
      the image name.
    - The default image tag is no more 'latest' but the current server version if
      no connection with Docker HUB is possible.
    - If the target version tag is not found on Docker HUB, then 'latest' is used
      as usual.

--- server/src/main/java/run/var/teamcity/cloud/docker/util/OfficialAgentImageResolver.java
+++ server/src/main/java/run/var/teamcity/cloud/docker/util/OfficialAgentImageResolver.java
@@ -1,34 +1,35 @@
 package run.var.teamcity.cloud.docker.util;
 
 import com.intellij.openapi.diagnostic.Logger;
-import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.version.ServerVersionHolder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import run.var.teamcity.cloud.docker.DockerImageConfig;
 import run.var.teamcity.cloud.docker.DockerImageDefaultResolver;
 import run.var.teamcity.cloud.docker.DockerImageNameResolver;
 import run.var.teamcity.cloud.docker.client.DockerClientException;
+import run.var.teamcity.cloud.docker.client.DockerClientFactory;
 import run.var.teamcity.cloud.docker.client.DockerRegistryClient;
 import run.var.teamcity.cloud.docker.client.DockerRegistryClientFactory;
 
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Utility class to resolve the TeamCity agent official images tag name.
  * <p>
- *     In order to keep agent upgrade/downgrade time as low as possible, we should select the newest agent image
- *     matching this server version. The algorithm implemented here rely on the assumption that the image version tag
- *     uses the server major/minor version of the server as prefix, followed with the version of the image itself.
+ *     In order to keep agent upgrade/downgrade time as low as possible, we should always use the agent image matching
+ *     this server version. The algorithm implemented here rely on the assumption that the official image version tag
+ *     uses the server version string (excluding the EAP marker).
  * </p>
  * <p>
- *     At this time, the Docker remote API does not permits to search for image tags. Therefore, we must perform the
- *     lookup ourselves on the Docker Hub registry (which is not guaranteed to succeed, depending for example, of
- *     network related restrictions).
+ *     This resolver will attempt to connect the Docker HUB registry in order to know if the resulting version tag
+ *     exists. If not, the {@code latest} tag will be used instead. This check is ignored the connection with the
+ *     Docker registry is possible (eg. because of network related restrictions).
  * </p>
  * <p>
- *     If no valid version tag could be resolved, the {@code latest} agent image will always be returned. the
- *     resolution result will be cached indefinitely.
+ *     The resolution result will be cached indefinitely.
  * </p>
  * <p>
  *     This class is thread-safe.
@@ -39,12 +40,11 @@ public class OfficialAgentImageResolver extends DockerImageNameResolver {
     private final static Logger LOG = DockerCloudUtils.getLogger(OfficialAgentImageResolver.class);
     final static String REPO = "jetbrains/teamcity-agent";
     static { assert !REPO.contains(":"): "Repository name must NOT contains a tag name"; }
-    final static String DEFAULT = REPO + ":latest";
-    private final static Pattern EXPECTED_VERSION_PTN = Pattern.compile("(?:\\d+\\.)*\\d+");
+    final static String LATEST = REPO + ":latest";
 
     private final ReentrantLock lock = new ReentrantLock();
-    private final int serverMajorVersion;
-    private final int serverMinorVersion;
+    private final String version;
+
     private final DockerRegistryClientFactory registryClientFty;
 
     private String imageTag;
@@ -52,20 +52,17 @@ public class OfficialAgentImageResolver extends DockerImageNameResolver {
     /**
      * Creates a new resolved for the specified major and minor server version.
      *
-     * @param serverMajorVersion the server major version
-     * @param serverMinorVersion the server minor version
+     * @param version the server major version
+     * @param registryClientFty the Docker registry client factory to be used to query the available tags
      *
      * @throws IllegalArgumentException if any version number is negative
      */
-    public OfficialAgentImageResolver(int serverMajorVersion, int serverMinorVersion,
-                                      DockerRegistryClientFactory registryClientFty) {
+    public OfficialAgentImageResolver(String version, DockerRegistryClientFactory registryClientFty) {
         super(new DockerImageDefaultResolver());
+        DockerCloudUtils.requireNonNull(version, "Version string cannot be null.");
         DockerCloudUtils.requireNonNull(registryClientFty, "Registry client factory cannot be null.");
-        if (serverMajorVersion < 0 || serverMinorVersion < 0) {
-            throw new IllegalArgumentException("Invalid version: " + serverMajorVersion + "." + serverMinorVersion);
-        }
-        this.serverMajorVersion = serverMajorVersion;
-        this.serverMinorVersion = serverMinorVersion;
+
+        this.version = version;
         this.registryClientFty = registryClientFty;
     }
 
@@ -105,81 +102,46 @@ public class OfficialAgentImageResolver extends DockerImageNameResolver {
 
         if (tags == null) {
             LOG.info("No remote version tags available, using default.");
-            return DEFAULT;
+            return REPO + ":" + version;
         } else {
             LOG.info("Found remote version tags: " + tags);
         }
 
-        String foundTag = null;
-        int[] foundTagVersionTokens = null;
+        boolean foundMatchingTag = false;
 
         for (Node tag : tags.getArray("tags").getArrayValues()) {
-            String tagValue = tag.getAsString();
-            int[] tagVersionTokens = parseVersion(tagValue);
-            if (tagVersionTokens == null || tagVersionTokens.length < 2) {
-                LOG.debug("Ignoring unparseable tag: " + tagValue);
-                continue;
-            }
-            if (tagVersionTokens[0] == serverMajorVersion && tagVersionTokens[1] == serverMinorVersion) {
-                if (foundTagVersionTokens == null || compare(tagVersionTokens, foundTagVersionTokens) > 0) {
-                    foundTag = tagValue;
-                    foundTagVersionTokens = tagVersionTokens;
-                }
+            if (version.equals(tag.getAsString())) {
+                foundMatchingTag = true;
             }
         }
 
-        if (foundTag == null) {
-            LOG.info("No match found in remote tags. Using default.");
-            return DEFAULT;
+        if (foundMatchingTag)  {
+            return REPO + ":" + version;
         }
 
-        return this.imageTag = REPO + ":" + foundTag;
+        return LATEST;
     }
 
-
-    private int[] parseVersion(String version) {
-        assert version != null;
-        if (EXPECTED_VERSION_PTN.matcher(version).matches()) {
-            String[] tokens = version.split("\\.");
-            int[] intTokens = new int[tokens.length];
-            for (int i = 0; i < tokens.length; i++) {
-                intTokens[i] = Integer.parseInt(tokens[i]);
-            }
-            return intTokens;
-        }
-        return null;
-    }
-
-    private int compare(int[] versionTokens1, int[] versionTokens2) {
-        assert versionTokens1 != null && versionTokens2 != null;
-        assert versionTokens1.length >= 2 && versionTokens2.length >= 2;
-
-        for (int i = 2; i < versionTokens1.length && i < versionTokens2.length; i++) {
-            int cmp = Integer.compare(versionTokens1[i], versionTokens2[i]);
-            if (cmp != 0) {
-                return cmp;
-            }
-        }
-
-        // Exited loop without finding a difference: a token is the version prefix of the other.
-        // The longest is then the newest.
-        return Integer.compare(versionTokens1.length, versionTokens2.length);
-    }
+    private static final Pattern TC_DISPLAY_VERSION_PTN = Pattern.compile("([0-9]+(?:\\.[0-9]+)*)(?:\\s.*)?");
 
     /**
-     * Creates a resolver for the provided build server instance.
+     * Creates a resolver for the current build server instance.
      *
-     * @param server the build server
+     * @param registryFty the Docker registry client factory
      *
      * @return the resolver
      *
-     * @throws NullPointerException if {@code server} is {@code null}
+     * @throws NullPointerException if {@code registryFty} is {@code null}
      */
     @NotNull
-    public static OfficialAgentImageResolver forServer(@NotNull SBuildServer server) {
-        DockerCloudUtils.requireNonNull(server, "Server instance cannot be null.");
+    public static OfficialAgentImageResolver forCurrentServer(DockerRegistryClientFactory registryFty,
+                                                              DockerClientFactory clientFty) {
+        DockerCloudUtils.requireNonNull(registryFty, "Registry client factory cannot be null.");
+        String version = ServerVersionHolder.getVersion().getDisplayVersion();
+        Matcher m = TC_DISPLAY_VERSION_PTN.matcher(ServerVersionHolder.getVersion().getDisplayVersion());
+        boolean matches = m.matches();
+        assert matches: "Unparseable server version: " + version;
 
-        return new OfficialAgentImageResolver(server.getServerMajorVersion(), server.getServerMinorVersion(),
-                DockerRegistryClientFactory.getDefault());
+        return new OfficialAgentImageResolver(m.group(1), registryFty);
     }
 }
