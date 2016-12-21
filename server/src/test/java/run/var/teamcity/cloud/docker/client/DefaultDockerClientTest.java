fda6a451cf3ece381ce62a0766216c9bc2c5227c
Jean Revertera <jean.revertera@gmail.com>
2016-12-21 05:32:24 +0100

    DefaultDockerClientTest: increased conn pool size

--- server/src/test/java/run/var/teamcity/cloud/docker/client/DefaultDockerClientTest.java
+++ server/src/test/java/run/var/teamcity/cloud/docker/client/DefaultDockerClientTest.java
@@ -93,7 +93,7 @@ public abstract class DefaultDockerClientTest {
     @Test
     @SuppressWarnings("ConstantConditions")
     public void attachAndLogs() throws URISyntaxException, IOException {
-        DefaultDockerClient client = createClient(3);
+        DefaultDockerClient client = createClient(10);
 
         Node containerSpec = Node.EMPTY_OBJECT.editNode().
                 put("Image", TEST_IMAGE).
