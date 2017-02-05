c6a2a3784c64e6a6b281a6dea423ec78e84808e1
Jean Revertera <jean.revertera@gmail.com>
2017-02-05 13:40:34 +0100

    0.3.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.apache.tools.ant.filters.ReplaceTokens
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.bundling.Zip
 
-version = "0.3.2-SNAPSHOT"
+version = "0.3.3-SNAPSHOT"
 group = "var.run.docker.cloud"
 val commitId = gitCommitId()
 allprojects {
