052af3a4e9a66ecc4acdcc5ebf40c1f4e090b619
Jean Revertera <jean.revertera@gmail.com>
2017-04-08 16:33:38 +0200

    Release 0.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.apache.tools.ant.filters.ReplaceTokens
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.bundling.Zip
 
-version = "0.3.4-SNAPSHOT"
+version = "0.3.4"
 group = "var.run.docker.cloud"
 val commitId = gitCommitId()
 allprojects {
