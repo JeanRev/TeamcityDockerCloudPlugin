526866e244adacf9500c9ef2a8d2faa89fb2067b
Jean Revertera <jean.revertera@gmail.com>
2017-05-06 16:19:25 +0200

    Settings: test for image migration

--- server/src/test/js/docker-cloud.test.js
+++ server/src/test/js/docker-cloud.test.js
@@ -1,7 +1,9 @@
 
 
 var dockerCloud = BS.Clouds.Docker;
+
 var $j = jQuery.noConflict();
+
 describe('Version number comparator', function() {
 
     it('should order valid version numbers', function() {
@@ -24,5 +26,74 @@ describe('Version number comparator', function() {
     });
 });
 
+describe('Migrating images data', function() {
 
+    it('should migrate empty image to latest version', function() {
+       var image = {
+           Administration: { Version: 1 }
+       };
+       dockerCloud._migrateImagesData(image);
+        expect(image.Administration.Version).toEqual(dockerCloud.IMAGE_VERSION);
+    });
 
+    it('should ignore images with unknown version', function() {
+        var image = {
+            Administration: { Version: 999999 }
+        };
+        dockerCloud._migrateImagesData(image);
+        expect(image.Administration.Version).toEqual(999999);
+    });
+
+    it('perform volume binding conversion when image V1', function() {
+        var hostConfig = {};
+        var editor = {};
+        var image = {
+            Administration: { Version: 1 },
+            Container: { HostConfig: hostConfig },
+            Editor: editor
+        };
+
+        hostConfig.Binds = ['/tmp/host_path:/tmp/container_path:rw', '/tmp/host_path2:volume:ro'];
+
+        dockerCloud._migrateImagesData(image);
+
+        expect(editor.Binds).toEqual([
+            { PathOnHost: '/tmp/host_path', PathInContainer: '/tmp/container_path', ReadOnly: 'rw' },
+            { PathOnHost: '/tmp/host_path2', PathInContainer: 'volume', ReadOnly: 'ro' }
+        ]);
+
+        expect(image.Administration.Version).toEqual(dockerCloud.IMAGE_VERSION);
+
+        image = {
+            Administration: { Version: 1 },
+            Container: { HostConfig: hostConfig },
+            Editor: editor
+        };
+
+        hostConfig.Binds = ['C:\\host_path:C:\\container_path:rw', 'C:\\host_path:volume:ro'];
+
+        dockerCloud._migrateImagesData(image);
+
+        expect(editor.Binds).toEqual([
+            { PathOnHost: 'C:\\host_path', PathInContainer: 'C:\\container_path', ReadOnly: 'rw' },
+            { PathOnHost: 'C:\\host_path', PathInContainer: 'volume', ReadOnly: 'ro' }
+        ]);
+    });
+
+    it('init default PullOnCreate flag when image V2', function() {
+        var image = {
+            Administration: { Version: 2 }
+        };
+
+        dockerCloud._migrateImagesData(image);
+
+        expect(image.Administration.PullOnCreate).toEqual(true);
+        expect(image.Administration.Version).toEqual(dockerCloud.IMAGE_VERSION);
+
+        image.Administration.PullOnCreate = false;
+
+        dockerCloud._migrateImagesData(image);
+
+        expect(image.Administration.PullOnCreate).toEqual(false);
+    });
+});
