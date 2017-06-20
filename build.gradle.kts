e671a498d87a4a5e11eeca611e274af87b29abb0
Jean Revertera <jean.revertera@gmail.com>
2017-06-20 16:06:42 +0200

    0.4.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.apache.tools.ant.filters.ReplaceTokens
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.bundling.Zip
 
-version = "0.4.1"
+version = "0.4.2-SNAPSHOT"
 group = "var.run.docker.cloud"
 val commitId = gitCommitId()
 allprojects {
